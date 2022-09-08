531d1f976e70bd914b44dd8c0eafa5a92aa7e5de
Boris Verdeyen Pazmiño <boris.verdeyen@i-logs.com>
2022-09-08 10:33:01 +0200

    Ability to see the booking uuid

--- backend/views/booking/view.php
+++ backend/views/booking/view.php
@@ -25,7 +25,7 @@ $this->params['breadcrumbs'] = [
 <?= DetailView::widget([
     'model' => $model,
     'attributes' => [
-        // 'created_at:datetime', 
+        'uuid', 
         'firstname', 
         'lastname', 
         'email',
